c5a208a8911d4545ad349b809f44ee03674e2f18
Eszter Kozma <kozma_eszter@yahoo.com>
2018-01-21 18:15:14 +0200

    Update8

--- cinema/src/main/java/ro/sci/cinema/dao/CinemaDAO.java
+++
@@ -1,9 +0,0 @@
-package ro.sci.cinema.dao;
-
-import ro.sci.cinema.domain.Cinema;
-import java.util.Collection;
-
-public interface CinemaDAO extends BaseDAO<Cinema> {
-
-    Collection<Cinema> searchByName(String query);
-}
